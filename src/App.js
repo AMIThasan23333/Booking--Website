3d30c1cff16af0da5d7868370b9e1d151c737bf2
My Name <mdarfinji45@gmail.com>
2023-10-18 11:08:01 +0800

    initial setup done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Lists from "./Pages/List/Lists";
+import Hotel from './Pages/Hotel/Hotel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <BrowserRouter>
+    
+    <Routes>
+
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/hotels" element={<Lists/>}></Route>
+      <Route path="/hotels/:id" element={<Hotel/>}></Route>
+    </Routes>
+
+    
+    </BrowserRouter>
+
+
+
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Navbar.css'
+const Navbar = () => {
+    return (
+        <div>
+         s   
+        </div>
+    );
+};
+
+export default Navbar;
